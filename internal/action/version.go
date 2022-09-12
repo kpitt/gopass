001223d6a0c21f448d2f35c6f968aa818ca6cb44
Kenny Pitt <kenny.pitt@gmail.com>
2022-09-12 08:09:15 -0400

    Remove mount versions from `version` command
    A `version` command is generally expected to be just a simple print of
    the application version.  In particular, it should not trigger the
    auto-sync that occurs when loading the `gopass` configuration in order
    to show the command versions for the configured mounts.  Instead, the
    version info for the mount points is now displayed by a separate
    `mounts versions` sub-command.

--- internal/action/version.go
+++ internal/action/version.go
@@ -1,57 +1,15 @@
 package action
 
 import (
-	"context"
 	"fmt"
-	"strings"
 
-	"github.com/blang/semver/v4"
-	"github.com/kpitt/gopass/internal/backend"
-	"github.com/kpitt/gopass/pkg/ctxutil"
 	"github.com/urfave/cli/v2"
 )
 
 // Version prints the gopass version.
 func (s *Action) Version(c *cli.Context) error {
-	ctx := ctxutil.WithGlobalFlags(c)
-
-	_ = s.IsInitialized(c)
-
 	cli.VersionPrinter(c)
 	fmt.Fprintln(stdout)
 
-	cryptoVer := versionInfo(ctx, s.Store.Crypto(ctx, ""))
-	storageVer := versionInfo(ctx, s.Store.Storage(ctx, ""))
-
-	tpl := "%-10s - %10s - %10s\n"
-	fmt.Fprintf(stdout, tpl, "<root>", cryptoVer, storageVer)
-
-	// report all used crypto, sync and fs backends.
-	for _, mp := range s.Store.MountPoints() {
-		cv := versionInfo(ctx, s.Store.Crypto(ctx, mp))
-		sv := versionInfo(ctx, s.Store.Storage(ctx, mp))
-
-		if cv != cryptoVer || sv != storageVer {
-			fmt.Fprintf(stdout, tpl, mp, cv, sv)
-		}
-	}
-
-	fmt.Fprintln(stdout)
-	fmt.Fprintf(stdout, "Available Crypto Backends: %s\n", strings.Join(backend.CryptoRegistry.BackendNames(), ", "))
-	fmt.Fprintf(stdout, "Available Storage Backends: %s\n", strings.Join(backend.StorageRegistry.BackendNames(), ", "))
-
 	return nil
-}
-
-type versioner interface {
-	Name() string
-	Version(context.Context) semver.Version
-}
-
-func versionInfo(ctx context.Context, v versioner) string {
-	if v == nil {
-		return "<none>"
-	}
-
-	return fmt.Sprintf("%s %s", v.Name(), v.Version(ctx))
 }
