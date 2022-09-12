2513cf54bc30181e00e833103bc0519ddb02e53b
Kenny Pitt <kenny.pitt@gmail.com>
2022-09-12 08:37:27 -0400

    Remove the `env` command
    An environment variable for a single secret can be set just as easily
    using command substitution.  The value of setting variables for all
    secrets in a directory seems a bit dubious, especially since multiple
    secrets with the same name in different sub-directories will collide on
    the same variable name, so a value may not come from the secret you
    expect.

--- main_test.go
+++ main_test.go
@@ -56,7 +56,6 @@ var commandsWithError = set.Map([]string{
 	".create",
 	".delete",
 	".edit",
-	".env",
 	".find",
 	".fscopy",
 	".fsmove",
@@ -121,7 +120,7 @@ func TestGetCommands(t *testing.T) { //nolint:paralleltest
 	c.Context = ctx
 
 	commands := getCommands(act, app)
-	assert.Equal(t, 39, len(commands))
+	assert.Equal(t, 38, len(commands))
 
 	prefix := ""
 	testCommands(t, c, commands, prefix)
